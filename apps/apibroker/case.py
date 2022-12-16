2631e355c193c6b8522dc6cf01feef32d78bc845
victor <victor.oliveira.costa.1987@gmail.com>
2022-12-16 10:21:10 +0000

    dry

--- apps/apibroker/case.py
+++ apps/apibroker/case.py
@@ -11,6 +11,7 @@ from xml.dom.minidom import parseString  # pretty xml
 
 logger = logging.getLogger(__name__)
 
+
 class CaseHelper():
     """
     CRUD operations goes here...
@@ -21,15 +22,15 @@ class CaseHelper():
         """
         Insert a new case into database by task
         """
-        user = User.objects.get(id=kwargs['owner'])
-        binary = bytes(base64.b64decode(kwargs['case_file']))
         try:
+            user = User.objects.get(id=kwargs['owner'])
+            binary = bytes(base64.b64decode(kwargs['case_file']))
             case = Case.objects.create(client_key=kwargs['client_key'], customer_key=kwargs['customer_key'], case_number=kwargs['case_number'],
-                                       plate_number=kwargs['plate_number'], preshared_key=kwargs['preshared_key'], owner=user, binary=binary)
+                                     plate_number=kwargs['plate_number'], preshared_key=kwargs['preshared_key'], owner=user, binary=binary)
             logger.info(f'Caso com id: {case.id} criado com sucesso...')
         except Exception as e:
             logger.error(str(e))
-            
+
     # Data Retrieve
     def get_case_list(*args, **kwargs):
         if kwargs['user'].role == 1:
@@ -71,6 +72,7 @@ class CaseHelper():
             case = None
             return case
 
+
 class CaseCore(CaseHelper):
     """
     Core Methods goes here...
@@ -83,11 +85,10 @@ class CaseCore(CaseHelper):
         """
         try:
             #xml_pretty = parseString(xml_bytes).toprettyxml()
-            #xml_pretty_decoded = str(xml_pretty).encode('utf-8')
-            xml_decoded = str(bytes(dicttoxml(
-                kwargs['dict'], root=False, attr_type=False)).decode('utf-8')).encode('utf-8')
-            base64_xml_with_doc_filter = base64.b64encode(
-                xml_decoded).decode("utf-8")
+            # xml_pretty_decoded = str(xml_pretty).encode(kwargs['kwargs['file_enconding']'])
+            xml_convert = dicttoxml(kwargs['dict'], root=False, attr_type=False)
+            xml_decoded = str(xml_convert.decode("utf-8")).encode(kwargs['file_enconding'])
+            base64_xml_with_doc_filter = base64.b64encode(xml_decoded).decode(kwargs['file_enconding'])
             return base64_xml_with_doc_filter
         except:
             logger.error('Conversion Dict to Xml Failed...')
@@ -101,11 +102,12 @@ class CaseCore(CaseHelper):
         try:
             #json_attachments = json.dumps(kwargs['dict'], indent = 4,  ensure_ascii=False).encode('utf8')
             json_attachments = json.dumps(
-                kwargs['dict'], ensure_ascii=False).encode('utf8')
-            decoded = json_attachments.decode("utf-8")
-            json_with_doc_filter_encode = str(decoded).encode("utf-8")
+                kwargs['dict'], ensure_ascii=False).encode(kwargs['file_enconding'])
+            decoded = json_attachments.decode(kwargs['file_enconding'])
+            json_with_doc_filter_encode = str(
+                decoded).encode(kwargs['file_enconding'])
             base64_json_with_doc_filter = base64.b64encode(
-                json_with_doc_filter_encode).decode("utf-8")
+                json_with_doc_filter_encode).decode(kwargs['file_enconding'])
             return base64_json_with_doc_filter
         except:
             logger.error('Conversion Dict to Json Failed...')
@@ -118,8 +120,9 @@ class CaseCore(CaseHelper):
         """
         try:
             decoded = base64.b64decode(binary)  # Data Validation
+            file_type, file_enconding = data_type(decoded)  # Data Validation
             encoded = base64.b64encode(decoded).decode(
-                "utf-8")  # Data Validation
+                file_enconding)  # Data Validation
             return encoded == binary
         except Exception as e:
             logger.error(str(e))
@@ -133,116 +136,112 @@ class CaseCore(CaseHelper):
         return [element for element in dict if element[key].lower() == doc_type.lower()]
 
     # Data Conversion
-    def convert_file_into_dict(file_type, xml_mime, file_enconding, json_mime, **kwargs):
-        if file_type in xml_mime:
+    def convert_file_into_dict(**kwargs):
+        if kwargs['file_type'] in kwargs['xml_mime']:
             try:
                 dict = xmltodict.parse(
-                    kwargs['binary'], encoding=file_enconding)  # Data Conversion
+                    kwargs['binary'], encoding=kwargs['file_enconding'])  # Data Conversion
                 return dict, True
             except Exception as e:
                 logger.error('Conversion XML to DICT Failed')
                 return False, str(e)
-        elif file_type in json_mime:
+        elif kwargs['file_type'] in kwargs['json_mime']:
             try:
                 data_json = bytes(kwargs['binary']).decode(
-                    file_enconding)  # Data Conversion
+                    kwargs['file_enconding'])  # Data Conversion
                 dict = json.loads(data_json)  # Data Conversion
                 return dict, True
             except Exception as e:
                 logger.error('Conversion JSON to DICT Failed')
                 return False, str(e)
         else:
-            return False, f"Allowed types {xml_mime} and {json_mime}"
+            return False, f"Allowed types {kwargs['xml_mime']} and {kwargs['json_mime']}"
 
     # Data Generation
-    def case_without_attachment(request, output, dict):
-        if request.method == "GET":
-            try: 
-                del dict['Case']['Message']['Attachments']
-                if output is not None and 'xml' == output.lower():
-                    base64_xml = CaseSystem.to_xml(
-                        dict=dict)  # Data Conversion
+    def case_without_attachment(**kwargs):
+        if kwargs['request'].method == "GET":
+            try:
+                del kwargs['dict']['Case']['Message']['Attachments']
+                if kwargs['output'] is not None and 'xml' == kwargs['output'].lower():
+                    base64_xml = CaseSystem.to_xml(**kwargs)  # Data Conversion
                     return base64_xml, None
-                elif output is None or output.lower() == 'json':
-                    base64_json = CaseSystem.to_json(dict=dict)
+                elif kwargs['output'] is None or kwargs['output'].lower() == 'json':
+                    base64_json = CaseSystem.to_json(**kwargs)
                     return base64_json, None
                 else:
-                    return False, "output must be json or xml"
-            except: 
+                    return False, "kwargs['output'] must be json or xml"
+            except:
                 return False, None
         else:
             return False, "Use GET method..."
-        
+
     # Data Generation
-    def case_with_all_attachment(request, output, dict):
-        if request.method == "POST": 
-            if output is not None and 'xml' == output.lower():
-                base64_xml = CaseSystem.to_xml(
-                    dict=dict)  # Data Conversion
+    def case_with_all_attachment(**kwargs):
+        if kwargs['request'].method == "POST":
+            if kwargs['output'] is not None and 'xml' == kwargs['output'].lower():
+                base64_xml = CaseSystem.to_xml(**kwargs)  # Data Conversion
                 return base64_xml, None
-            elif output is not None and 'json' == output.lower():
-                base64_json = CaseSystem.to_json(
-                    dict=dict)  # Data Conversion
+            elif kwargs['output'] is not None and 'json' == kwargs['output'].lower():
+                base64_json = CaseSystem.to_json(**kwargs)  # Data Conversion
                 return base64_json, None
             else:
-                return False, "output value must be (xml or json)"
+                return False, "kwargs['output'] value must be (xml or json)"
         else:
             return False, "Use POST method..."
 
     # Data Generation
-    def case_with_attachment_filter(request, doc_type, dict, output):
-        if request.method == "POST":
-            if doc_type and not doc_type.lower() == "all":
-                if (dict['Case']['Message']['Attachments']['Attachment']):
+    def case_with_attachment_filter(**kwargs):
+        if kwargs['request'].method == "POST":
+            if kwargs['doc_type'] and not kwargs['doc_type'].lower() == "all":
+                if (kwargs['dict']['Case']['Message']['Attachments']['Attachment']):
                     list_of_attachments = CaseSystem.search(
-                        doc_type, dict['Case']['Message']['Attachments']['Attachment'], "doc_type")
+                        kwargs['doc_type'], kwargs['dict']['Case']['Message']['Attachments']['Attachment'], "doc_type")
                 else:
                     return False, "No attachments available"
-                try: 
-                    del dict['Case']['Message']
+                try:
+                    del kwargs['dict']['Case']['Message']
                     if list_of_attachments:
-                        dict['Case']['Message'] = {'Attachments': {'Attachment': list_of_attachments}}
-                        if output is not None and 'xml' == output.lower():
-                            base64_xml = CaseSystem.to_xml(
-                                dict=dict)  # Data Conversion
+                        kwargs['dict']['Case']['Message'] = {'Attachments': {
+                            'Attachment': list_of_attachments}}
+                        if kwargs['output'] is not None and 'xml' == kwargs['output'].lower():
+                            base64_xml = CaseSystem.to_xml(**kwargs)  # Data Conversion
                             return base64_xml, None
-                        elif output is None or 'json' == output.lower():
-                            base64_json = CaseSystem.to_json(
-                                dict=dict)  # Data Conversion
+                        elif kwargs['output'] is None or 'json' == kwargs['output'].lower():
+                            base64_json = CaseSystem.to_json(**kwargs)  # Data Conversion
                             return base64_json, None
                         else:
-                            return False, "output must be json or xml"
+                            return False, "kwargs['output'] must be json or xml"
                     else:
                         return False, "No results"
-                except: 
+                except:
                     return False, None
-            elif doc_type and doc_type.lower() == "all":
-                dict['Case']['Message'] = {'Attachments': {'Attachment': dict['Case']['Message']['Attachments']['Attachment']}}
-                if output is not None and output.lower() == 'xml':
-                    base64_xml = CaseSystem.to_xml(
-                        dict=dict)  # Data Conversion
+            elif kwargs['doc_type'] and kwargs['doc_type'].lower() == "all":
+                kwargs['dict']['Case']['Message'] = {'Attachments': {
+                    'Attachment': kwargs['dict']['Case']['Message']['Attachments']['Attachment']}}
+                if kwargs['output'] is not None and kwargs['output'].lower() == 'xml':
+                    base64_xml = CaseSystem.to_xml(**kwargs)  # Data Conversion
                     return base64_xml, None
-                elif output is None or output.lower() == 'json':
-                    base64_json = CaseSystem.to_json(dict=dict)
+                elif kwargs['output'] is None or kwargs['output'].lower() == 'json':
+                    base64_json = CaseSystem.to_json(**kwargs) # Data Conversion
                     return base64_json, None
                 else:
-                    return False, "output must be json or xml"
+                    return False, "kwargs['output'] must be json or xml"
             else:
                 return False, "Use GET for all attachments..."
-        if request.method == "GET":
-            dict['Case']['Message'] = {'Attachments': {'Attachment': dict['Case']['Message']['Attachments']['Attachment']}}
-            if output is not None and output.lower() == 'xml':
-                base64_xml = CaseSystem.to_xml(
-                    dict=dict)  # Data Conversion
+        if kwargs['request'].method == "GET":
+            kwargs['dict']['Case']['Message'] = {'Attachments': {
+                'Attachment': kwargs['dict']['Case']['Message']['Attachments']['Attachment']}}
+            if kwargs['output'] is not None and kwargs['output'].lower() == 'xml':
+                base64_xml = CaseSystem.to_xml(**kwargs)  # Data Conversion
                 return base64_xml, None
-            elif output is None or output.lower() == 'json':
-                base64_json = CaseSystem.to_json(dict=dict)
+            elif kwargs['output'] is None or kwargs['output'].lower() == 'json':
+                base64_json = CaseSystem.to_json(**kwargs) # Data Conversio
                 return base64_json, None
             else:
-                return False, "output must be json or xml"
+                return False, "kwargs['output'] must be json or xml"
         else:
             return False, "Use POST or GET method..."
-        
+
 class CaseSystem(CaseCore):
 
     # Data Generation Core
@@ -254,24 +253,24 @@ class CaseSystem(CaseCore):
         """
         xml_mime = ['application/xml', 'text/xml']
         json_mime = ['application/json']
+
         # Return mimetype and file encoding
         file_type, file_enconding = data_type(kwargs['binary'])
-        doc_type, output, request = kwargs['doc_type'], kwargs['output'], kwargs['request']
-    
+        
         # Convert Xml or Json to python dict with the right codec from original file
-        dict, msg = CaseSystem.convert_file_into_dict(file_type, xml_mime, file_enconding, json_mime, **kwargs)
+        dict, msg = CaseSystem.convert_file_into_dict(
+            file_type=file_type, xml_mime=xml_mime, file_enconding=file_enconding, json_mime=json_mime, **kwargs)
 
         # Attachment filter
-        if(dict):
+        if (dict):
             if kwargs['attachment'] is None:
-                result, msg = CaseSystem.case_with_all_attachment(request, output, dict)
+                result, msg = CaseSystem.case_with_all_attachment(dict=dict, file_enconding=file_enconding, **kwargs)
                 return result, msg
             elif kwargs['attachment']:
-                result, msg = CaseSystem.case_with_attachment_filter(request, doc_type, dict, output)
+                result, msg = CaseSystem.case_with_attachment_filter(dict=dict, file_enconding=file_enconding, **kwargs)
                 return result, msg
             elif not kwargs['attachment']:
-                result, msg = CaseSystem.case_without_attachment(request, output, dict)
+                result, msg = CaseSystem.case_without_attachment(dict=dict, file_enconding=file_enconding, **kwargs)
                 return result, msg
         else:
             return False, msg
-            
